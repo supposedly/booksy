0ebdc6e24fa7794668780b448518397f9e050f2b
Hadi <hdtrhn@gmail.com>
2017-12-11 19:47:48 -0800

    THIS CAN NOT BE THE INTENDED WAY TO DO THINGS

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
-import asyncio
-import sanic
+import sanic, asyncio, uvloop
 import os
 from sanic import response
 
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 app = sanic.Sanic(__name__)
 
 @app.route('/')
-async def test(req):
-    return response.text('Hello world!')
+async def test(request):
+    return response.html('''<link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+<link rel="manifest" href="/icons/manifest.json">
+<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="/icons/favicon.ico">
+<meta name="apple-mobile-web-app-title" content="Booksy">
+<meta name="application-name" content="Booksy">
+<meta name="msapplication-config" content="/icons/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+<text>Hello world!</text>''')
 
-print(os.environ)
 app.run(host='0.0.0.0', port=os.environ.get('PORT', 8000), debug=True)
