357af02b1fe1903ab40d04692ce2c132ec1d3283
Eli <eltrhn@gmail.com>
2017-12-31 20:17:14 -0800

    Still not working. Going to give up on aiofiles for now (probably not even necessary tbh because I'm literally just reading these tiny text files)

--- backend/setup.py
+++ backend/setup.py
@@ -4,6 +4,67 @@ import asyncio
 from functools import wraps
 from .typedef import User
 
+QUERY = """
+CREATE TABLE IF NOT EXISTS
+  locations (
+    lid BIGSERIAL PRIMARY KEY,
+    name TEXT,
+    ip TEXT UNIQUE,
+    fine_amt NUMERIC DEFAULT 0.10,
+    fine_interval INT DEFAULT 1,
+    media_types TEXT[] DEFAULT '{book, other}',
+    color SMALLINT,
+    image BYTEA
+  );
+CREATE TABLE IF NOT EXISTS 
+  members (
+    uid BIGSERIAL PRIMARY KEY,
+    username TEXT,
+    lid BIGINT,
+    UNIQUE(username, lid),
+    fullname TEXT,
+    email TEXT,
+    phone TEXT,
+    manages BOOL DEFAULT false,
+    rid BIGINT,
+    checkouts BIGINT,
+    pwhash bytea,
+    type SMALLINT,
+    maxes BIGINT,
+    locks BIGINT
+  );
+CREATE TABLE IF NOT EXISTS
+  items (
+    mid BIGSERIAL UNIQUE PRIMARY KEY,
+    type TEXT,
+    isbn TEXT,
+    lid BIGINT,
+    title TEXT,
+    author TEXT,
+    published DATE,
+    genre TEXT,
+    issued_to BIGINT,
+    due_date DATE,
+    fines NUMERIC,
+    acquired TIMESTAMP,
+    maxes BIGINT
+  );
+CREATE TABLE IF NOT EXISTS
+  holds (
+    mid BIGINT,
+    uid BIGINT,
+    created TIMESTAMP,
+    PRIMARY KEY(mid, uid)
+  );
+CREATE TABLE IF NOT EXISTS
+  roles (
+    rid BIGSERIAL UNIQUE PRIMARY KEY,
+    lid BIGINT,
+    name TEXT,
+    permissions SMALLINT,
+    maxes BIGINT,
+    locks BIGINT
+  );
+"""
 async def create_pg_tables(conn):
-    async with aiofiles.open('/app/backend/sql/schema_setup.sql', 'r') as f:
-        await conn.execute(await f.read())
+    await conn.execute(QUERY)
--- backend/typedef.py
+++ backend/typedef.py
@@ -9,6 +9,86 @@ from asyncio import iscoroutinefunction as is_coro
 from .core import LazyProperty, AsyncInit, lockquire
 from .resources import Perms, Maxes, Locks
 
+# aiofiles isn't working for some reason so screw it I'll just do this
+REGISTER_LOCATION = """
+INSERT INTO locations (
+              name, ip,
+              color, image
+            )
+     SELECT $1::text, $2::text,
+            $2::smallint, $3::bytea;
+
+-- ROLES SETUP --
+
+INSERT INTO roles (
+              lid, name,
+              permissions, 
+              maxes, locks
+              )
+     SELECT currval(pg_get_serial_sequence('locations', 'lid')),
+            'Admin'::text,
+            32767::smallint, -- maximum smallint value, so every permission bc admin
+            9223372036854775807::bigint, -- maximum bigint value, so no maxes
+            9223372036854775807::bigint; -- maximum bigint value, so no locks
+
+INSERT INTO roles (
+              lid, name, 
+              permissions, 
+              maxes, locks
+              )
+     SELECT currval(pg_get_serial_sequence('locations', 'lid')),
+            'Organizer'::text,
+            55::smallint, -- 0110111
+            1028::bigint, -- 4, 4, 0...
+            5140::bigint; -- 20, 20, 0 . . .
+
+INSERT INTO roles (
+              lid, name, 
+              permissions, 
+              maxes, locks
+              )
+     SELECT currval(pg_get_serial_sequence('locations', 'lid')),
+            'Subscriber'::text,
+            0::smallint, -- 0000000
+            514::bigint, -- 2, 2, 0 . . .
+            5135::bigint; -- 15, 15, 0 . . .
+
+-- END ROLES SETUP --
+
+-- ACCOUNTS SETUP --
+
+INSERT INTO members (
+              username, pwhash,
+              lid, rid,
+              fullname,
+              manages, type
+              )
+     SELECT $4::text, $5::bytea, -- checkout acct; username determined by backend (usually school initials plus '-checkout-XX' [unique digits])
+            currval(pg_get_serial_sequence('locations', 'lid')),
+            currval(pg_get_serial_sequence('roles', 'rid')),
+            locations.name + ' Patron',
+            false, 1
+       FROM locations
+      WHERE lid = currval(pg_get_serial_sequence('locations', 'lid'))
+
+INSERT INTO members (
+              username, pwhash,
+              lid, rid,
+              fullname, email, phone,
+              manages, type
+              )
+     SELECT $6::text, $7::bytea, -- admin acct; username determined by backend (usually school initials + '-admin')
+            currval(pg_get_serial_sequence('locations', 'lid')),
+            currval(pg_get_serial_sequence('roles', 'rid')),
+            $8::text, $9::text, $10::text,
+            true, 0;
+
+-- END ACCOUNTS SETUP --
+
+SELECT currval(pg_get_serial_sequence('locations', 'lid'))
+"""
+# aiofiles isn't working for some reason so screw it I'll just do this
+
 class MediaType(AsyncInit):
     _aiolock = asyncio.Lock()
     props = [
@@ -223,9 +303,8 @@ class Location(AsyncInit):
           'admin_name', 'admin_email', 'admin_phone'
           ]
         args = [kwargs[attr] for attr in props]
-        async with self.__class__._aiolock, aiofiles.open('./sql/register_location.sql') as f:
-            query = ''.join(await f.readlines())
-            lid = await conn.fetchval(query, *args) # returns LID
+        async with self.__class__._aiolock:
+            lid = await conn.fetchval(REGISTER_LOCATION, *args) # returns LID
         return cls(lid, self.app)
     
     @classmethod
