2528f849fc088be3fdb3e190cefa11d9922fa45b
Eli <hdtrhn@gmail.com>
2018-01-08 20:56:53 -0800

    Add sidebar toggle

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,15 +2,22 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    uID: string;
-    lID: string;
-    rID: string;
-    locname: string;
-    username: string;
-    name: string;
-    email: string;
-    phone: string;
-    managesLocation: boolean;
-    isCheckoutAccount: boolean;
-    isLoggedIn: boolean = false;
+  // info regarding currently-logged-in user
+  uID: string;
+  lID: string;
+  rID: string;
+  locname: string;
+  username: string;
+  name: string;
+  email: string;
+  phone: string;
+  managesLocation: boolean;
+  isCheckoutAccount: boolean;
+  isLoggedIn: boolean = false;
+  
+  // whether to hide the homepage sidebar.
+  // It's here instead of in sidebar.component.ts so that the
+  // setting persists even when the user switches to/from any
+  // of the three main tabs.
+  hide: boolean = false;
 }
