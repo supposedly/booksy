d1bed163755b7c8a314016424bf17debbedd1715
Eli <hdtrhn@gmail.com>
2018-01-13 23:27:46 -0800

    Add help view component

---
+++ backend/blueprints/api/help.py
@@ -0,0 +1,28 @@
+"""/help"""
+# This is the only one of these API files that uses straight Postgres
+# instead of outsourcing it to typedef.py. Sorta interesting I guess.
+
+import asyncpg
+import sanic
+import sanic_jwt as jwt
+from sanic_jwt import decorators as jwtdec
+
+from .import uid_get, rqst_get
+from .import Location, Role, MediaType, MediaItem, User
+
+help = sanic.Blueprint('api_help', url_prefix='/help')
+
+@help.get('/titles')
+async def serve_help_titles(rqst):
+    """These can be cached, because they won't change during a session"""
+    async with rqst.app.acquire() as conn:
+        titles = await conn.fetch("""SELECT id, title FROM help WHERE true""")
+    return sanic.response.json([{'id': i['id'], 'title': i['title']} for i in titles], status=200)
+
+@help.get('/article')
+@rqst_get('id')
+async def serve_help_article(rqst, id_):
+    """These should never be cached; too big"""
+    async with rqst.app.acquire() as conn:
+        text = await conn.fetchval("""SELECT content FROM help WHERE id = $1::bigint""", id_)
+    return sanic.response.json({'text': text}, status=200)
