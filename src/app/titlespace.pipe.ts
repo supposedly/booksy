87d90cfac6972778151877e99e56a3a44e61b683
Hadi <hdtrhn@gmail.com>
2018-05-12 13:47:37 -0700

    Fix titlespace pipe

--- src/app/titlespace.pipe.ts
+++ src/app/titlespace.pipe.ts
@@ -5,6 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'titlespace'})
 export class SpaceOnlyTitleCasePipe implements PipeTransform {
   transform(string: string): string {
+    if (string == null) {
+      return string;
+    }
     return string.split(' ')
       .map(s => s[0].toUpperCase() + s.slice(1))
       .join(' ');
